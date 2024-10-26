bb5e530070bb8714a6f5d93fe74a5b29fd9b9519
mohammed hossam <mohammedhossam5000@gmail.com>
2024-10-26 11:45:04 +0000

    modfying according to requirements

--- markdown2html.py
+++ markdown2html.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+markdown2html.py
+
+A script to convert a Markdown file to HTML.
+Usage:
+    ./markdown2html.py input_markdown_file output_html_file
+"""
+
 import sys
 import os
 import markdown
 
+
 def main():
+    """
+    Main function to handle argument checks, file checks, and conversion of
+    Markdown content to HTML format.
+    """
     # Check if the correct number of arguments is provided
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
@@ -19,14 +32,15 @@ def main():
         sys.exit(1)
 
     # Convert Markdown to HTML and write to the output file
-    with open(markdown_file, 'r') as md_file:
+    with open(markdown_file, 'r', encoding='utf-8') as md_file:
         markdown_content = md_file.read()
         html_content = markdown.markdown(markdown_content)
 
-    with open(html_file, 'w') as output_file:
+    with open(html_file, 'w', encoding='utf-8') as output_file:
         output_file.write(html_content)
 
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
